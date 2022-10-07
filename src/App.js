cc8017e7f793e2d6495224c54cfe9914f8ec0042
hamidehp <hamideh_p@yahoo.com>
2022-10-07 21:47:04 +0330

    hook useRef

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
+import Accordion from './Accordion';
 import UseRefExample from './hooks/UseRefExample';
+import UseMemoExample from './hooks/UseMemExample';
 const App=() =>{
   return (
     <div className="container">
@@ -8,7 +10,11 @@ const App=() =>{
      <h2>آموزش استفاده از هوک های ری اکت</h2>
      </div>
      <hr className='text-danger' />
+     <Accordion title="useREf Hook " heading ="headingOne">
      <UseRefExample />
+     </Accordion>
+     <hr className='text-primary' />
+    
     </div>
   );
 }
---
+++ src/hooks/UseMemExample.jsx
@@ -0,0 +1,4 @@
+const UseMemoExample =() =>{
+
+}
+export default UseMemoExample;
