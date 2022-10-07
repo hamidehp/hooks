26df2d83b3482fb878d3b94826d195e73ceffa59
hamidehp <hamideh_p@yahoo.com>
2022-10-07 21:27:21 +0330

    useRef

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,4 +1,21 @@
-const Accordion= ()=>{
-
+const Accordion= ({children,title,heading})=>{
+<div className="accordion" id="accordianHooks">
+    <div className="accordian-item">
+        <h2 className="accordian-header" id={heading}>
+            <button className="accordion-button collapsed"
+             type="button"
+             data-bs-toggle="collapse"
+             data-bs-expanded="true"
+             aria-controls={`collapse${heading}`}
+             style={{fontFamily:"vazir"}}>{title}</button>
+        </h2>
+        <div id={`collapse${heading}`}
+             className="accordion-collapse collapse"
+             aria-labelledby={heading}
+             dats-bs-parent="#according Hooks">
+                <div className="according-body">{children}</div>
+             </div>
+    </div>
+</div>
 }
 export default Accordion;
